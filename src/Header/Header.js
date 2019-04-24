ac4b8f441e44745f82bbd0737d2adbb9a0861f9c
Cole Vanacore <colevanacore@Coles-MBP.lan>
2019-04-24 13:50:43 -0400

    Setup header component

---
+++ src/Header/Header.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './Header.scss'
+
+class Header extends Component {
+    constructor() {
+        super()
+      }
+
+    render() {
+        return (
+            <div>
+                Cole Vanacore
+            </div>
+        )
+    }
+}
+
+export default Header;
