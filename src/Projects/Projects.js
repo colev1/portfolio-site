50b86a75ffdf0f37bc1ce7b96ffbda34e9f7c6eb
Cole Vanacore <colevanacore@gmail.com>
2019-05-04 21:45:17 -0400

    Create articles to contain title and image for each project

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -9,21 +9,38 @@ class Projects extends Component {
     render() {
         return (
             <div className="projects-container">
-              <article className="project-1">
+              <article>
                   <h3>  AdoptR </h3>
-                  <a href="https://github.com/colev1/cross-poll-fe"> GitHub repo </a>
+                  <div className="project project-1">
+                    <a href="https://github.com/colev1/cross-poll-fe">
+                      <i class="fab fa-github"></i> github repo
+                    </a>
+                  </div>
               </article>
-              <article className="project-2">
+              <article>
                 <h3>Palette Picker </h3>
-                <a href="https://github.com/colev1/palette-picker"> GitHub repo </a>
+                <div className="project project-2">
+                  <a href="https://github.com/colev1/palette-picker">
+                    <i class="fab fa-github"></i> 
+                    github repo 
+                  </a>
+                </div>
               </article>
-              <article className="project-3">
+              <article>
                 <h3> Movie Tracker</h3>
-                <a href="https://github.com/colev1/movie-tracker"> GitHub repo </a>
+                <div className="project project-3">
+                  <a href="https://github.com/colev1/movie-tracker"> 
+                    <i class="fab fa-github"></i> github repo 
+                  </a>
+                </div>
               </article>
-              <article className="project-4">
+              <article>
                 <h3>Swapi Box </h3>
-                <a href="https://github.com/colev1/swapi-box"> GitHub repo </a>
+                <div className="project project-4">
+                  <a href="https://github.com/colev1/swapi-box"> 
+                    <i class="fab fa-github"></i> github repo 
+                  </a>
+                </div>
               </article>
             </div>
         )
